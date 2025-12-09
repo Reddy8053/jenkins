717b1d88f9cd89e597d5c5ca19fdf8f1f42adcff
Reddy8053 <deekshithreddy8053@gmail.com>
2025-12-09 17:40:50 -0600

    Initialize simple Node.js app with Express

---
+++ simple-node-app/src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Simple Node.js CI app is running' });
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+module.exports = app;
